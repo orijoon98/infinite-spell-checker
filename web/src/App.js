84796c5a579f339cbe9697eedae6727ac1ff8a59
HyeokJoon Kong <orijoon98@gmail.com>
2022-11-13 20:46:18 +0900

    [web/feat] 로그인 페이지 추가, 라우터 설정 추가

--- web/src/App.js
+++ web/src/App.js
@@ -1,5 +1,14 @@
+import { Route, Routes } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+
 function App() {
-    return <></>;
+    return (
+        <>
+            <Routes>
+                <Route path="/" element={<LoginPage />} />
+            </Routes>
+        </>
+    );
 }
 
 export default App;
---
+++ web/src/containers/LoginContainer.js
@@ -0,0 +1,7 @@
+import Login from "../components/Login";
+
+const LoginContainer = () => {
+    return <Login />;
+};
+
+export default LoginContainer;
