5b9b55e33cd557615dd65fabb35762c51043d33e
HyeokJoon Kong <orijoon98@gmail.com>
2022-11-13 20:51:15 +0900

    [web/feat] 전체 공통 스타일 추가

--- web/src/App.js
+++ web/src/App.js
@@ -1,9 +1,21 @@
 import { Route, Routes } from "react-router-dom";
+import { createGlobalStyle } from "styled-components";
 import LoginPage from "./pages/LoginPage";
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    padding: 0;
+    margin: 0;
+  }
+  body {
+    word-break: keep-all;
+  }
+`;
 
 function App() {
     return (
         <>
+            <GlobalStyle />
             <Routes>
                 <Route path="/" element={<LoginPage />} />
             </Routes>
