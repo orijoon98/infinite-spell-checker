b1fde4e012318ef438a7b1793547223f745fa63b
HyeokJoon Kong <orijoon98@gmail.com>
2022-11-14 10:26:47 +0900

    [web/feat] 로그인 페이지 디자인 추가

--- web/src/components/Login.js
+++ web/src/components/Login.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import kakao_button_image from "../assets/kakao_login_large_wide.png";
+import kakao_button_image from "../assets/images/kakao_login_large_wide.png";
 
 const Login = ({}) => {
     const CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
@@ -10,15 +10,35 @@ const Login = ({}) => {
     return (
         <>
             <LoginContainer>
-                <a href={KAKAO_AUTH_URL}>
-                    <KakaoButton></KakaoButton>
-                </a>
+                <LoginWrapper>
+                    <LoginTitle>
+                        <h1>무한 맞춤법 검사기</h1>
+                    </LoginTitle>
+                    <a href={KAKAO_AUTH_URL}>
+                        <KakaoButton></KakaoButton>
+                    </a>
+                    <p>소셜 로그인으로 간편하게 시작하세요</p>
+                </LoginWrapper>
             </LoginContainer>
         </>
     );
 };
 
-const LoginContainer = styled.div``;
+const LoginContainer = styled.div`
+    text-align: center;
+    width: 100vw;
+    height: 100vh;
+    font-family: "line";
+`;
+
+const LoginTitle = styled.div`
+    margin-top: 200px;
+    margin-bottom: 100px;
+`;
+
+const LoginWrapper = styled.div`
+    margin: auto;
+`;
 
 const KakaoButton = styled.div`
     background-image: url(${kakao_button_image});
