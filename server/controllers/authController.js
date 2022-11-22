3dfd5e11276bf6431a0d407bff9a16f101ede1e4
HyeokJoon Kong <orijoon98@gmail.com>
2022-11-22 22:09:20 +0900

    [server/fix] 쿠키에 담는 data 수정

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -3,7 +3,7 @@ const authService = require("../services/authService");
 exports.login = async (req, res, next) => {
     try {
         const data = await authService.login(req);
-        return res.status(200).cookie("accessToken", data.data).json({
+        return res.status(200).cookie("accessToken", data).json({
             success: true,
             message: "로그인 성공",
             data: data,
